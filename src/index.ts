660f04fde2296292dd6b9cf4d7c3ea6cc3b9d04c
Una L <una.lestrange@atu.ie>
2024-09-12 14:23:46 +0100

    end of task 1 on ws1

---
+++ src/index.ts
@@ -0,0 +1,25 @@
+
+
+import express, {Application} from "express" ;
+
+
+
+
+const PORT = process.env.PORT || 3000;
+
+const app: Application = express();
+
+
+
+app.get("/ping", async (_req, res) => {
+    res.json({
+    message: "hello from Una",
+    });
+});
+
+
+app.listen(PORT, () => {
+    console.log("Server is running on port", PORT);
+    });
+
+
