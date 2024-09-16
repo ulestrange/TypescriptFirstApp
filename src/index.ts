4047fba1de2c0c707c275bdf23fe0b5bc3b3cdb6
Una L <una.lestrange@atu.ie>
2024-09-16 21:45:57 +0100

    added user routes with dummy controllers

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,20 @@
 
+/* external imports */
 
 import express, {Application, Request, Response} from "express" ;
 
 import morgan from "morgan";
 
+/* internal imports */
+
+import userRoutes from './routes/users';
 
 const PORT = process.env.PORT || 3000;
 
 const app: Application = express();
 
 app.use(morgan("tiny"));
+app.use(express.json());
 
 app.get("/ping", async (_req : Request, res: Response) => {
     res.json({
@@ -22,8 +27,10 @@ app.get('/bananas', async (_req : Request, res: Response) =>
   res.send('hello world, this is bananas - ha ha ha ha hda ha '));
 
 
+app.use('/api/v1/users', userRoutes)
+
 app.listen(PORT, () => {
-    console.log("Server is running on port", PORT);
+    console.log("Server is running on port  --", PORT);
     });
 
 
---
+++ src/routes/users.ts
@@ -0,0 +1,18 @@
+import express, {Router} from 'express';
+import {
+  getUsers,
+  getUserById,
+  createUser,
+  updateUser,
+  deleteUser,
+} from '../controllers/users';
+
+const router: Router = express.Router();
+
+router.get('/', getUsers);
+router.get('/:id', getUserById);
+router.post('/', createUser);
+router.put('/:id', updateUser);
+router.delete('/:id', deleteUser);
+
+export default router;
