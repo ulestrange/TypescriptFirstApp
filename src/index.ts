0b87f13cb0faed82b5e8e5a1af46322663316d80
Una L <una.lestrange@atu.ie>
2024-09-16 08:57:18 +0100

    added morgan and request response types

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,25 @@
 
 
-import express, {Application} from "express" ;
+import express, {Application, Request, Response} from "express" ;
 
-
+import morgan from "morgan";
 
 
 const PORT = process.env.PORT || 3000;
 
 const app: Application = express();
 
+app.use(morgan("tiny"));
 
-
-app.get("/ping", async (_req, res) => {
+app.get("/ping", async (_req : Request, res: Response) => {
     res.json({
     message: "hello from Una - has this changed",
     });
 });
+
+
+app.get('/bananas', async (_req : Request, res: Response) =>
+  res.send('hello world, this is bananas - ha ha ha ha hda ha '));
 
 
 app.listen(PORT, () => {
