80be103850d0c954b4ed8c1b7bbebde166711de7
Una L <una.lestrange@atu.ie>
2024-09-20 09:48:01 +0100

    just checking if the header is there

--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,7 @@ import morgan from "morgan";
 /* internal imports */
 
 import userRoutes from './routes/users';
+import {authenticateKey} from './middleware/auth';
 
 const PORT = process.env.PORT || 3000;
 
@@ -28,7 +29,7 @@ app.get("/ping", async (_req : Request, res: Response) => {
 app.get('/bananas', async (_req : Request, res: Response) =>
   res.send('hello world, this is bananas - ha ha ha ha hda ha '));
 
-  app.use('/api/v1/users', userRoutes)
+app.use('/api/v1/users',  userRoutes)
 
 
 app.listen(PORT, () => {
