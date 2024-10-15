731f152db3dc4ae500498bec665e3030b61e3653
Una L <una.lestrange@atu.ie>
2024-10-15 14:18:40 +0100

    stuff happens

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@
 import express, { Application, Request, Response } from "express";
 
 import morgan from "morgan";
-import cors from 'cors';
+import cors from "cors";
 
 import dotenv from "dotenv";
 
@@ -25,15 +25,15 @@ const app: Application = express();
 app.use(morgan("tiny"));
 app.use(express.json());
 
+// const corsOptions = {
+//   origin: ['http://localhost:4200'],
+//   optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+// }
 
-const corsOptions = {
-  origin: ['http://localhost:4200'],
-  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
-}
+const corsOptions = {};
+//app.use(cors<Request>(corsOptions)); // this is too braod need to narrow
 
-
-
-app.use(cors<Request>(corsOptions)); // this is too braod need to narrow
+app.use(cors());
 
 //app.use(authenticateKey); // all the routes below
 
@@ -53,4 +53,3 @@ app.use("/api/v1/gradeHistories", gradeHistoriesRoutes);
 app.listen(PORT, () => {
   console.log("Server is running on port ready for deploy 1 --", PORT);
 });
-
