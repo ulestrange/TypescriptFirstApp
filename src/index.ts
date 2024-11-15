7eac9608a31636e3fb15b4b300a800b91ab953c2
Una L <una.lestrange@atu.ie>
2024-11-15 15:12:45 +0000

    Put grade history now changes the entire grade history

--- src/index.ts
+++ src/index.ts
@@ -4,11 +4,13 @@
 import express, {Application, Request, Response} from "express" ;
 
 import morgan from "morgan";
+import cors from "cors";
 
 
 import dotenv from "dotenv";
 
 import { Db} from 'mongodb';
+
 
 dotenv.config();
 
@@ -26,6 +28,7 @@ const app: Application = express();
 
 app.use(morgan("tiny"));
 app.use(express.json());
+app.use(cors());
 
 //app.use(authenticateKey); - all the routes below
 
@@ -36,8 +39,7 @@ app.get("/ping", async (_req : Request, res: Response) => {
 });
 
 
-app.get('/bananas', async (_req : Request, res: Response) =>
-  res.send('hello world, this is bananas - ha ha ha ha hda ha '));
+
 
 app.use('/api/v1/users',  userRoutes)
 app.use('/api/v1/gradeHistories',  gradeHistoriesRoutes)
