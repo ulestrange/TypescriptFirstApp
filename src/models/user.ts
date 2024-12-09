b512736e938e45f0f43440cb2bf7b35b85850875
Una L <una.lestrange@atu.ie>
2024-12-09 14:43:43 +0000

    added role stuff

--- src/models/user.ts
+++ src/models/user.ts
@@ -1,11 +1,13 @@
 import { ObjectId } from "mongodb";
 import Joi from "joi";
 
+
+enum role {admin, editor, ''}
 export interface User {
   name: string;
   phonenumber: string;
   email: string;
-  role?: string;
+  role?: role;
   id?: ObjectId;
   dateJoined?: Date;
   lastUpdated?: Date;
@@ -17,7 +19,7 @@ export const ValidateUser = (user: User) => {
   const userJoiSchema = Joi.object<User>({
     name: Joi.string().min(3).required(),
     phonenumber: Joi.string().min(10),
-    role: Joi.string(),
+    role: Joi.string().valid(...Object.values(role)),
     email: Joi.string().email().required(),
     password: Joi.string().min(8).max(64).required(),
   });
